fc3d65fbd754c43f003e1992acbb8d1f248a6b4e
Willian Silva <silva.williansantos@gmail.com>
2020-09-30 12:07:44 -0300

    Create Resolvers User and Post

--- src/graphql/modules/users/resolvers.js
+++ src/graphql/modules/users/resolvers.js
@@ -1,4 +1,13 @@
+import User from '../../../models/User';
+
 export default {
-  Query: {},
-  Mutation: {},
+  Query: {
+    users: () => User.find(),
+    user: (_, { id }) => User.findById(id),
+  },
+  Mutation: {
+    createUser: (_, { data }) => User.create(data),
+    updateUser: (_, { id, data }) => User.findOneAndUpdate(id, data, { new: true }),
+    deleteUser: async (_, { id }) => !!(await User.findOneAndDelete(id)),
+  },
 };
