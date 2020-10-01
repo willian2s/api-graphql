6629fc91813ac324320054298696ddf0141e5cb1
Willian Silva <silva.williansantos@gmail.com>
2020-10-01 15:32:45 -0300

    Add methods en resolvers User, Post

--- src/graphql/modules/posts/resolvers.js
+++ src/graphql/modules/posts/resolvers.js
@@ -1,6 +1,10 @@
 import Post from '../../../models/Post';
+import User from '../../../models/User';
 
 export default {
+  Post: {
+    author: (post) => User.findById(post.author),
+  },
   Query: {
     posts: () => Post.find(),
     post: (_, { id }) => Post.findById(id),
--- src/graphql/modules/users/resolvers.js
+++ src/graphql/modules/users/resolvers.js
@@ -1,6 +1,9 @@
 import User from '../../../models/User';
 
 export default {
+  User: {
+    fullName: (user) => `${user.firstName} ${user.lastName}`,
+  },
   Query: {
     users: () => User.find(),
     user: (_, { id }) => User.findById(id),
